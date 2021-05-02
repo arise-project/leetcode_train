a8805db7d0cd68f90f9487f443d2308899ff780d
Yevhen Pyrohov <Evgeny.Pirogov@aspose.com>
2021-05-02 23:40:39 +0300

    large factorial

---
+++ leetcode_train/hk/fact.cs
@@ -0,0 +1,97 @@
+class Result
+{
+
+        public static BigInteger ipow(int b, int exp)
+        {
+            BigInteger result = 1;
+            BigInteger bigb = b;
+            for (; ; )
+            {
+                if ((exp & 1) == 1)
+                    result *= bigb;
+                exp >>= 1;
+                if (exp == 0)
+                    break;
+                bigb *= bigb;
+            }
+
+            return result;
+        }
+
+        public static bool isPrime(int n)
+        {
+            // Corner cases
+            if (n <= 1)
+                return false;
+            if (n <= 3)
+                return true;
+
+            // This is checked so
+            // that we can skip
+            // middle five numbers
+            // in below loop
+            if (n % 2 == 0 ||
+                n % 3 == 0)
+                return false;
+
+            for (int i = 5;
+                     i * i <= n; i = i + 6)
+                if (n % i == 0 ||
+                    n % (i + 2) == 0)
+                    return false;
+
+            return true;
+        }
+
+        static IEnumerable<int> primeList(int n)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                if (isPrime(i))
+                    yield return i;
+            }
+        }
+
+        public static int d(int n, int k)
+        {
+            int sum = 0;
+
+            int pow = k;
+            int quotient = 1;
+            while (quotient > 0)
+            {
+                quotient = n / pow;
+                sum += quotient;
+                pow*=k;
+            }
+
+            return sum;
+        }
+
+
+        public static BigInteger factorailPrime(int n)
+        {
+            var primes = primeList(n);
+            var expo = primes.Select(p => d(n, p)).ToArray();
+            var multi = primes.Select((p, i) => ipow(p, expo[i])).ToArray();
+            BigInteger result = new BigInteger(1);
+            for(int i=0; i<multi.Length;i++)
+            {
+                result *= multi[i];
+            }
+
+            return result;
+        }
+        
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n)
+    {
+        Console.WriteLine(factorailPrime(n));
+    }
+
+}
